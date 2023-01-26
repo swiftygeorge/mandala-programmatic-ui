574f436b37736303312ecbcb1f19ea8d63e546f8
george mapaya <119117609+swiftygeorge@users.noreply.github.com>
2023-01-26 14:59:45 -0500

    Initial Commit

---
+++ MandalaProgrammaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MandalaProgrammaticUI
+//
+//  Created by George Mapaya on 2023-01-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
